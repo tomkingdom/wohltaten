7a155b9efb24810c7f22530cf8cc7bb3b9d47786
tomkingdom <tomkingdom@gmail.com>
2025-01-11 21:29:49 +0100

    footnotes experiment

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,43 @@
+
 const { DateTime } = require("luxon");
 
 // add to .eleventy.js
 module.exports = function(eleventyConfig) {
+
+  /* Markdown Overrides */
+  // set markdown footnote processor
+  let markdownIt = require("markdown-it");
+  let markdownItFootnote = require("markdown-it-footnote");
+
+  let options = {
+    html: true, // Enable HTML tags in source
+    breaks: true,  // Convert '\n' in paragraphs into <br>
+    linkify: true // Autoconvert URL-like text to links
+  };
+  
+  // configure the library with options
+  let markdownLib =  markdownIt(options).use(markdownItFootnote);
+  // set the library to process markdown files
+  
+  markdownLib.renderer.rules.footnote_caption = (tokens, idx) => {
+    let n = Number(tokens[idx].meta.id + 1).toString();
+  
+    if (tokens[idx].meta.subId > 0) {
+      n += ":" + tokens[idx].meta.subId;
+    }
+  
+    return n;
+  };
+
+  eleventyConfig.setLibrary("md", markdownLib);
+
+  //rest of your config here...
     eleventyConfig.addPassthroughCopy("src/assets/css/style.css");
     eleventyConfig.addPassthroughCopy("src/assets/css/tailwind.css");
     eleventyConfig.addPassthroughCopy("src/assets/images");
     eleventyConfig.addPassthroughCopy("src/admin/index.html");
     eleventyConfig.addPassthroughCopy("src/admin/config.yml");
-    let markdownIt = require("markdown-it");
-    let markdownItFootnote = require("markdown-it-footnote");
+
 
     eleventyConfig.addShortcode(
         "headers",
@@ -29,6 +58,7 @@ module.exports = function(eleventyConfig) {
           return a.data.order - b.data.order;
         });
       });
+
 
 
     return {
