8d50010793ef0e3f23dd02a7ee3cedfa17bab684
tomkingdom <tomkingdom@gmail.com>
2025-01-11 14:33:08 +0100

    nm

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
+const { DateTime } = require("luxon");
+
+// add to .eleventy.js
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/css/style.css");
     eleventyConfig.addPassthroughCopy("src/assets/images");
+    eleventyConfig.addPassthroughCopy("admin");
 
     eleventyConfig.addShortcode(
         "headers",
@@ -8,13 +12,22 @@ module.exports = function(eleventyConfig) {
           `<h1>${title}</h1>
             <p>${subtitle}</p>`
       );
+      eleventyConfig.addShortcode("currentDate", (date = DateTime.now()) => {
+        return date;
+      });  
+      
+      eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+      });
       
       eleventyConfig.addCollection("page", function(collections) {
         return collections.getFilteredByTag("page").sort(function(a, b) {
           return a.data.order - b.data.order;
         });
       });
-      
+
+        
+
     return {
       dir: {
         input: "src",
